96ff5dc0d330649f0e02ec1ceee08a446f8b39b3
page-dotcom <asepkonha@gmail.com>
2026-02-07 15:32:07 +0700

    Update pancingan.tsx

--- pages/pancingan.tsx
+++ pages/pancingan.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 
 export default function Pancingan() {
-  const blank = "⠀"; // Karakter invisible
+  const blank = "https://&#x200B;";
+  const title = "&#x200B;";
   const fakeDomain = "facebook.com"; // Domain pancingan di deskripsi
   const imgUrl = "https://your-image-link.com/photo.jpg"; // URL Gambar kamu
 
@@ -9,7 +10,7 @@ export default function Pancingan() {
     <>
       <Head>
         <title>{blank}</title>
-        <meta property="og:title" content={blank} />
+        <meta property="og:title" content={title} />
         <meta property="og:url" content={blank} />
         <meta property="og:description" content={fakeDomain} />
         <meta property="og:image" content={imgUrl} />
